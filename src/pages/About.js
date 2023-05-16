76f89e935815aed1eaf8ce533a9db52b551d73f1
Ali Ataya <ali@smilefish.com>
2023-05-15 20:24:54 -0700

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,20 @@
 import { Container } from "react-bootstrap";
 
+const facts = [
+  " In January 2020 , there were 580,466 people experiencing homelessness on our streets",
+  "7,877 deaths in 2020",
+];
+
 export const About = () => {
   return (
     <Container className="text-center">
-      <h1 className="m-5 ">Homelessness</h1>
-      <ul style={{ listStyle: "none", fontSize: 24, lineHeight: 5 }}>
-        <p>
-          In January 2020 , there were 580,466 people experiencing homelessness
-          on our streets{" "}
-        </p>
-        <p>7,877 deaths in 2020</p>
+      <h1 className="m-5">Homelessness</h1>
+      <ul style={{ listStyle: "none", fontSize: 24, lineHeight: 2 }}>
+        {facts.map((fact, index) => (
+          <li key={index} style={{ borderBottom: "3px solid black" }}>
+            {fact}
+          </li>
+        ))}
       </ul>
     </Container>
   );
