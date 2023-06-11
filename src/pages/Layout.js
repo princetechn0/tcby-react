d91629dc1be05dfeb184cdc70284c790304adb6b
Ali Ataya <ali@smilefish.com>
2023-06-10 20:08:54 -0700

    ui changes

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import CustomNavbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Layout = ({ loggedInUser }) => {
   return (
     <>
       <CustomNavbar loggedInUser={loggedInUser} />
       <Outlet />
+      <Footer />
     </>
   );
 };
