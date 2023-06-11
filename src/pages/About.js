4bbfa9c29ea33a36f54672274246933d96220a77
Ali Ataya <ali@smilefish.com>
2023-06-10 19:26:48 -0700

    about page fixed

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,37 +1,69 @@
 import { Container } from "react-bootstrap";
 import "../styles/About.css";
+import { Masonry } from "masonic";
+import data from "../data/homelessData";
 
-const facts = [
-  {
-    info: "In January 2020 , there were 580,466 people experiencing homelessness on our streets",
-    source:
-      "https://endhomelessness.org/homelessness-in-america/homelessness-statistics/state-of-homelessness/#key-facts",
-  },
-  {
-    info: "28% were people living in families with children",
-    source:
-      "https://endhomelessness.org/homelessness-in-america/homelessness-statistics/state-of-homelessness/",
-  },
-  { info: "7,877 deaths in 2020", source: "https://homelessdeathscount.org" },
-];
+const JumboCard = ({ title, text, source }) => {
+  const cardClasses = `p-5 bg-dark text-white rounded-3 hoverable`;
+
+  return (
+    <div className={cardClasses} onClick={() => window.open(source, "_blank")}>
+      <div className="container-fluid py-5">
+        <h1 className="display-5 fw-bold">{title}</h1>
+        <p className="col-md-10 fs-4">{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const StandardCard = ({ title, text, source }) => {
+  const cardClasses = `p-5 bg-light rounded-3 border border-dark hoverable`;
+
+  return (
+    <div className={cardClasses} onClick={() => window.open(source, "_blank")}>
+      <h2 className="fw-bold">{title}</h2>
+      <p className="col-md-10 fs-5">{text}</p>
+    </div>
+  );
+};
+
+const CustomCard = ({ data }) => {
+  if (data.type === "jumbo") {
+    return (
+      <JumboCard
+        key={data.header}
+        title={data.header}
+        text={data.info}
+        source={data.source}
+        buttonText={data.buttonText}
+      />
+    );
+  }
+  return (
+    <StandardCard
+      key={data.header}
+      title={data.header}
+      text={data.info}
+      source={data.source}
+      buttonText={data.buttonText}
+    />
+  );
+};
 
 export const About = () => {
   return (
-    <Container>
+    <Container className="my-5">
       <div className="about-container col-10">
         <h1 className="about-text">About</h1>
       </div>
-      {facts.map((elem) => (
-        <a
-          className="about-a"
-          href={elem.source}
-          target="_blank"
-          key={elem.info}
-          rel="noreferrer"
-        >
-          {elem.info}
-        </a>
-      ))}
+      <Container className="p-5 bg-light rounded-3">
+        <Masonry
+          items={data}
+          render={CustomCard}
+          columnGutter={60}
+          columnWidth={400}
+        />
+      </Container>
     </Container>
   );
 };
