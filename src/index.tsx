00c1a5d5c24076d3fa638f77f448ef896f5eb59c
Ali Ataya <appleluvver8@gmail.com>
2023-06-22 18:40:05 -0700

    tsx conversion

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
